d925048fdf9da641d8fc6ace9c990cbea4a89749
makirov03 <hajymammetmak03@gmail.com>
2025-02-21 15:46:16 +0500

    Fixed cmd

---
+++ cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"log"
+	"net/http"
+
+	"elibrary/internal/delivery/bookhandlers"
+	"elibrary/internal/delivery/userhandlers"
+	"elibrary/internal/repository/bookrepository"
+	"elibrary/internal/repository/userrepository"
+	"elibrary/internal/usecase/bookusecase"
+	"elibrary/internal/usecase/userusecase"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// Initialize repositories
+	dbConn, err := pgx.Connect(context.Background(), "postgres://atajan:atajansan@localhost:5432/postgres")
+	if err != nil {
+		log.Fatal("Unable to connect to database:", err)
+	}
+	bookRepo := bookrepository.NewBookRepository(dbConn)
+	userRepo := userrepository.NewUserRepository(dbConn)
+
+	// Initialize use cases
+	bookUC := bookusecase.NewBookUsecase(bookRepo)
+	userUC := userusecase.NewUserUsecase(userRepo)
+
+	// Initialize handlers
+	bookHandler := bookhandlers.NewBookHandler(bookUC)
+	userHandler := userhandlers.NewUserHandler(userUC)
+
+	// Book routes
+	r.HandleFunc("/books", bookHandler.GetBooksHandler).Methods("GET")
+	r.HandleFunc("/books/{id}", bookHandler.GetBooksHandler).Methods("GET")
+	r.HandleFunc("/books", bookHandler.UploadBookHandler).Methods("POST")
+	r.HandleFunc("/books/upload", bookHandler.UploadBookHandler).Methods("POST")
+
+	// User routes
+	r.HandleFunc("/users/register", userHandler.RegisterHandler).Methods("POST")
+	r.HandleFunc("/users/login", userHandler.LoginHandler).Methods("POST")
+
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
